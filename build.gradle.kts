0060b45442b239675a71d6cd4874986214ac2a6f
Dmitriy Derepko <xepozz@list.ru>
2025-06-06 17:31:14 +0300

    allow any top version

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,7 +79,6 @@ intellijPlatform {
 
         ideaVersion {
             sinceBuild = providers.gradleProperty("pluginSinceBuild")
-            untilBuild = providers.gradleProperty("pluginUntilBuild")
         }
     }
 
